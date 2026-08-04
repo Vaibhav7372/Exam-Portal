e8d84bc5378aedf86dc35323d396ad1d916b7533
Vaibhav-ingale88 <vaibhavingale88@gmail.com>
2026-08-04 18:03:21 +0530

    fix issue

--- ai_exam_portal/ai_exam_portal/settings.py
+++ ai_exam_portal/ai_exam_portal/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default="false").strip().lower() in {
+    "1",
+    "true",
+    "yes",
+    "on",
+}
 
 ALLOWED_HOSTS = [
     "localhost",
